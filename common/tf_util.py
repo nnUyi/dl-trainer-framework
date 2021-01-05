e0d6e806768f7502e2bc8e82b4bc406f725febe7
chadyang <computerscienceyyz@163.com>
2021-01-05 11:20:29 +0800

    add tf_util

---
+++ common/tf_util.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+# tf_util.py
+# chadyang <chadyouzyang@gmail.com>
+# Created time: 2021-01-05 10:58
+# Distributed under term of Myself
+
+# TODO (chadyang): add tf utils in this module
